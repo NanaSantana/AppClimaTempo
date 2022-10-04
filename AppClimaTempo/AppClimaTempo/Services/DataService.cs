1c4994f5cba662c4b2cdb61bac96832882cd9ad9
Usuario <Usuario@CPD02-M02>
2022-10-04 12:22:44 -0300

    add codigo na model e services

--- AppClimaTempo/AppClimaTempo/Services/DataService.cs
+++ AppClimaTempo/AppClimaTempo/Services/DataService.cs
@@ -2,9 +2,79 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AppClimaTempo.Model;
+
 namespace AppClimaTempo.Services
 {
-    internal class DataService
+    class DataService
     {
+        public static async Task<Tempo> GetPrevisaoDoTempo(string cidade)
+        {
+            string appId = "27f3dbe7780d6027b8150a6fd63dd8c1";
+
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&units=metric" + "&appid=" + appId;
+
+            dynamic resultado = await getDataFromService(queryString).ConfigureAwait(false);
+
+            if (resultado["weather"] != null)
+            {
+                Tempo previsao = new Tempo();
+                previsao.Title = (string)resultado["name"];
+                previsao.Temperature = (string)resultado["main"]["temp"] + " C";
+                previsao.Wind = (string)resultado["wind"]["speed"] + " mph";
+                previsao.Humidity = (string)resultado["main"]["humidity"] + " %";
+                previsao.Visibility = (string)resultado["weather"][0]["main"];
+
+                DateTime time = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)resultado["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)resultado["sys"]["sunset"]);
+
+                previsao.Sunrise = String.Format("{0:d/MM/yyyy HH:mm:ss}", sunrise);
+                previsao.Sunset = String.Format("{0:d/MM/yyyy HH:mm:ss}", sunset);
+                
+                return previsao;
+
+            }
+
+            else
+            {
+                return null;
+            }
+        }
+
+        public static async Task<dynamic> getDataFromService(string queryString)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(queryString);
+            dynamic data = null;
+
+            if(response != null)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject<dynamic>(json);
+            }
+            return data;
+        }
+
+        public static async Task<dynamic> getDataFromServiceByCity(string city)
+        {
+            string appId = "27f3dbe7780d6027b8150a6fd63dd8c1";
+
+            string url = string.Format("http://api.openweathermap.org/data/2.5/forecast/daily?q={0}&units=metric&cnt=1&APPID={1}", city.Trim(), appId);
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetAsync(url);
+            dynamic data = null;
+
+            if(response !== null)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject(json);
+            }
+            return data;
+        }
     }
 }
